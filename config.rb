04f0938238745648a2a5a0ec817021719dbe8082
Michael Koper <michaelkoper@gmail.com>
2017-05-03 14:34:59 +0200

    added social icons

--- config.rb
+++ config.rb
@@ -38,3 +38,12 @@ configure :build do
   # Enable cache buster (except for images)
   activate :asset_hash, ignore: [/\.jpg\Z/, /\.png\Z/]
 end
+
+helpers do
+  def svg(name)
+    root = Middleman::Application.root
+    file_path = "#{root}/source/images/#{name}.svg"
+    return File.read(file_path) if File.exists?(file_path)
+    '(not found)'
+  end
+end
